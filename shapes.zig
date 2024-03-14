09f7059f9de7c53f37bc4ad21659c9323a5a93d4
10aded <10aded-Streaming@protonmail.com>
2024-03-14 14:15:55 -0400

    more cleanup, this time shapes.zig

--- shapes.zig
+++ shapes.zig
@@ -1,3 +1,8 @@
+// This file just serves as an interface to calling raylib functions that
+// draw basic 2D geometry.
+//
+// Specifically, rectangles, triangles, and circles.
+
 const rl     = @cImport(@cInclude("raylib.h"));
 
 const Vec2   = @Vector(2, f32);
@@ -5,7 +10,6 @@ const Color = [4] u8;
 const Pixel = [4] u8;
 
 // Draw a plain (colored) rectangle, where the position determines the center.
-
 pub fn draw_centered_rect( pos : Vec2, width : f32, height : f32, color : Color) void {
     const top_left_x : i32 = @intFromFloat(pos[0] - 0.5 * width);
     const top_left_y : i32 = @intFromFloat(pos[1] - 0.5 * height);
@@ -15,7 +19,6 @@ pub fn draw_centered_rect( pos : Vec2, width : f32, height : f32, color : Color)
 // NOTE: Bloody raylib needs the point when drawing a triangle to be in
 // a counter-clockwise direction,... so draw each triangle with both directions.
 pub fn draw_triangle(p1 : Vec2, p2 : Vec2, p3 : Vec2, color : Color) void {
-    
     const dumb_p1 = rl.Vector2{ .x = p1[0], .y = p1[1]};
     const dumb_p2 = rl.Vector2{ .x = p2[0], .y = p2[1]};
     const dumb_p3 = rl.Vector2{ .x = p3[0], .y = p3[1]};
@@ -23,12 +26,13 @@ pub fn draw_triangle(p1 : Vec2, p2 : Vec2, p3 : Vec2, color : Color) void {
     rl.DrawTriangle(dumb_p2, dumb_p1, dumb_p3, rlc(color));
 }
 
-fn rlc(color : Color) rl.Color {
-    return rl.Color{.r = color[0], .g = color[1], .b = color[2], .a = color[3]};
-}
-
 pub fn draw_centered_circle( pos : Vec2, radius : f32, color : Color) void {
     const posx : c_int = @intFromFloat(pos[0]);
     const posy : c_int = @intFromFloat(pos[1]);
     rl.DrawCircle(posx, posy, radius, rl.Color{.r = color[0], .g = color[1], .b = color[2], .a = color[3]});
+}
+
+// Convert our color data type to raylib's color data type.
+fn rlc(color : Color) rl.Color {
+    return rl.Color{.r = color[0], .g = color[1], .b = color[2], .a = color[3]};
 }
